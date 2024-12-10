ddd210e84421fe8409cce0b19a867a911411e5ef
Amy472 <hang.vo@lc.cuny.edu>
2024-12-10 17:52:45 -0500

    Update project10.R

--- project10.R
+++ project10.R
@@ -1,2 +1,2 @@
-boxplot(vgsales$NA_Sales, main = "NA_Sales")
-boxplot(vgsales$JP_Sales, main = "JP_Sales")
+boxplot(vgsales$NA_Sales, main = "NA_Sales", main = "NA_Sales", ylab = "Sales (million copies of a video game sold)")
+boxplot(vgsales$JP_Sales, main = "JP_Sales", ylab = "Sales (Million copies of a video game sold)")
